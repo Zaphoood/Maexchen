afea0ef034498facd05ebcab42827fa4f8dfd889
Mathis Weber <mathis.c.weber@web.de>
2021-04-26 15:12:39 +0200

    Added add and sub methods for Throw

--- throw.py
+++ throw.py
@@ -2,20 +2,37 @@ import constants as c
 import random
 
 
+class OutOfBoundsError(Exception):
+    """Wird gemeldet, wenn eine Addition von Throw und int zu einem nicht definierten Wert
+    führen würde (z. B. größer als Mäxchen, kleiner als 31)"""
+
+
 class Throw:
-    num0: int
-    num1: int
     value: int
     rank: int
     isDouble: bool
     isMaexchen: bool
 
-    def __init__(self, num0, num1):
-        if not 1 <= num0 <= 6 and 1 <= num1 <= 6:
-            raise ValueError("Values must be elements of [1, 6].")
-        self.num0 = num0
-        self.num1 = num1
-        self.value = max(num0, num1) * 10 + min(num0, num1)
+    def __init__(self, *args: int):
+        if len(args) == 1:
+            # Ein Argument wurde gegeben, also der Wert des Wurfs (self.value)
+            self.value = args[0]
+            if self.value not in c.THROW_VALUES:
+                raise ValueError(f"Value of {self.value} is not valid.")
+            num0 = self.value // 10
+            num1 = self.value % 10
+        elif len(args) == 2:
+            # Zwei Argumente wurden gegeben, also die Ergebnisse der einzelnen Würfel
+            num0, num1 = args
+            if not 1 <= num0 <= 6 and 1 <= num1 <= 6:
+                raise ValueError("Values must be elements of [1, 6].")
+            num0 = num0
+            num1 = num1
+            self.value = max(num0, num1) * 10 + min(num0, num1)
+        else:
+            # Ungültige Anzahl an Argumenten
+            raise TypeError(f"Throw() takes either one or two arguments, got {len(args)}")
+
         self.rank = c.THROW_RANK_BY_VALUE[self.value]
         self.isDouble = num0 == num1  # Pasch
         self.isMaexchen = self.value == c.MAEXCHEN  # Mäxchen
@@ -43,7 +60,17 @@ class Throw:
         # Ist von kleinerem Rang
         return self.rank < other.rank
 
+    def __add__(self, other: int):
+        assert isinstance(other, int)
+        try:
+            return Throw(c.THROW_VALUES[self.rank + other])
+        except IndexError:
+            raise OutOfBoundsError
+
+    def __sub__(self, other: int):
+        return self.__add__(-other)
+
 
 def randomThrow():
-    # Gibt einen Zufälligen Wurf zurück
+    """Gibt einen zufälligen Wurf zurück"""
     return Throw(random.randint(1, 6), random.randint(1, 6))

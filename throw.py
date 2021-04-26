1af119cb50954ff5d412d815f8c6c9a8c25ec11f
Mathis Weber <mathis.c.weber@web.de>
2021-04-26 15:46:26 +0200

    Fixed behavior of Throw add and sub

--- throw.py
+++ throw.py
@@ -26,8 +26,6 @@ class Throw:
             num0, num1 = args
             if not 1 <= num0 <= 6 and 1 <= num1 <= 6:
                 raise ValueError("Values must be elements of [1, 6].")
-            num0 = num0
-            num1 = num1
             self.value = max(num0, num1) * 10 + min(num0, num1)
         else:
             # Ungültige Anzahl an Argumenten
@@ -61,10 +59,13 @@ class Throw:
         return self.rank < other.rank
 
     def __add__(self, other: int):
+        # Gibt das nte nächstgrößere Wurfergebnis zurück. n ist als Parameter gegeben.
+        # Meldet OutOfBoundsError, sollte dieses Ergebnis nicht existieren.
         assert isinstance(other, int)
-        try:
-            return Throw(c.THROW_VALUES[self.rank + other])
-        except IndexError:
+        newRank = self.rank + other
+        if 0 <= newRank < len(c.THROW_VALUES):
+            return Throw(c.THROW_VALUES[newRank])
+        else:
             raise OutOfBoundsError
 
     def __sub__(self, other: int):

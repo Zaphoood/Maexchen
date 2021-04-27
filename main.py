5ce918f729697ccf3b97a2a1a73f3838ced6d70d
Mathis Weber <mathis.c.weber@web.de>
2021-04-27 16:20:27 +0200

    Test game with dummies and show-offs in main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import throw
+from game import Game
+from player import DummyPlayer, ShowOffPlayer
+
+if __name__ == '__main__':
+    game = Game([*[DummyPlayer()] * 3, ShowOffPlayer()])
+
+    game.init()
+    game.run()

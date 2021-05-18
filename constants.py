0d312fc3a77747c62c3537097437b7c094c295cd
Mathis Weber <mathis.c.weber@web.de>
2021-05-18 17:09:10 +0200

    Clean up move.py and constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,3 @@
-from enum import Enum
-
 MAEXCHEN = 21
 
 # Alle möglichen Ergebnisse für einen Wurf (mit der höheren Zahl als erste Stelle),
@@ -7,27 +5,7 @@ MAEXCHEN = 21
 THROW_VALUES = [
     *[i * 10 + j for i in range(3, 7) for j in range(1, i)],  # "Normale" Würfe
     *[i * 10 + i for i in range(1, 7)],  # Päsche
-    21  # Mäxchen
+    MAEXCHEN
 ]
 # Weist jedem Wurfergebnisses einen Rang zu
 THROW_RANK_BY_VALUE = {val: rank for rank, val in enumerate(THROW_VALUES)}
-
-
-# TODO: Move this enum to move.py
-class ALL_MOVES(Enum):
-    """Alle möglichen Arten von Zügen die ein Spieler durchführen kann
-
-    Mögliche Werte sind
-    -DOUBT: Das Ergebnis des vorherigen Spielers anzweifeln
-    -THROW: Würfeln und das Ergebnis verkünden (Die Möglichkeit des Lügens ist hier beinhaltet)
-    """
-
-    DOUBT = 0
-    THROW = 1
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}.{self._name_}"
-
-    def __str__(self):
-        return f"{self._name_}"
-
--- move.py
+++ move.py
@@ -1,16 +1,34 @@
 from __future__ import annotations  # Notwendig für type hints die die eigene Klasse beinhalten
+from enum import Enum
 
-import constants as c
 from throw import Throw
+
+
+class ALL_MOVES(Enum):
+    """Alle möglichen Arten von Zügen die ein Spieler durchführen kann
+
+    Mögliche Werte sind
+    -DOUBT: Das Ergebnis des vorherigen Spielers anzweifeln
+    -THROW: Würfeln und das Ergebnis verkünden (Die Möglichkeit des Lügens ist hier beinhaltet)
+    """
+
+    DOUBT = 0
+    THROW = 1
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}.{self._name_}"
+
+    def __str__(self):
+        return f"{self._name_}"
 
 
 class Move:
     """Klasse die einen Zug beschreibt, der von einem Spieler durchgeführt wird"""
 
-    move: c.ALL_MOVES
+    move: ALL_MOVES
     value: Throw
 
-    def __init__(self, move: c.ALL_MOVES, value: Throw = None) -> None:
+    def __init__(self, move: ALL_MOVES, value: Throw = None) -> None:
         self.move = move  # Art des Zuges die dieser Zug darstellt (DOUBT oder THROW)
         self.value = value  # Wird nur für Züge verwendet, bei denen der Spieler würfelt.
         # Beinhaltet die Angabe des Spielers über das Würfelergebnis

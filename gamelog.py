d3b95cd8d80111fa821ad49997a2e29f7d1cd176
Mathis Weber <mathis.c.weber@web.de>
2021-08-27 17:23:03 +0200

    Add getEvents() method to GameLog

--- gamelog.py
+++ gamelog.py
@@ -30,6 +30,7 @@ class GameLog:
 
     def prettyList(self) -> list[str]:
         """Den Ablauf des Spiels schön formatiert als Liste von strings zurückgeben"""
+
         prettyList = [f"=== Game initialized with {self.n_players} player{'s' if self.n_players > 1 else ''}: ==="]
         prettyList.extend([f" - {str(player)}" for player in self.players])
         for i, round in enumerate(self.rounds):
@@ -44,13 +45,20 @@ class GameLog:
 
     def hasFinished(self):
         """Gibt zurück, ob das Spiel, zu dem dieser GameLog gehört, (ordnungsgemäß) beendet wurde"""
+
         with contextlib.suppress(IndexError):
             return isinstance(self.rounds[-1][-1], (EventFinish, EventAbort))
         return False
 
     def pretty(self) -> str:
         """Den von prettyList() Spielablauf als string zurückgeben"""
+
         return "\n".join(self.prettyList())
+
+    def getEvents(self) -> list[Event]:
+        """Alle Ereignisse des Spiels in einer flachen Liste zurückgeben"""
+
+        return [event for round in self.rounds for event in round]
 
     def __eq__(self, other: GameLog) -> bool:
         if not isinstance(other, GameLog):

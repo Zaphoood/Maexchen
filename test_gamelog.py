01b38b1d69490172050507b46c9636d7550ffc0f
Mathis Weber <mathis.c.weber@web.de>
2021-10-05 18:48:26 +0200

    Adjust unittest to reflect variable name changes

--- test_gamelog.py
+++ test_gamelog.py
@@ -7,7 +7,7 @@ from gameevent import EventAbort
 
 class TestGameLog(unittest.TestCase):
     def test_abort(self):
-        players = [DummyPlayer(id=1)]
+        players = [DummyPlayer(playerId=1)]
         log = GameLog(players)
         log.happen(EventAbort())
         pretty = log.pretty()
@@ -15,7 +15,7 @@ class TestGameLog(unittest.TestCase):
         self.assertEqual(len(pretty.split('\n')), 4)
 
     def test_player_list(self):
-        players = [DummyPlayer(id=1), ShowOffPlayer(id=2)]
+        players = [DummyPlayer(playerId=1), ShowOffPlayer(playerId=2)]
         log = GameLog(players)
         log.happen(EventAbort())
         pretty = log.pretty()
@@ -23,7 +23,7 @@ class TestGameLog(unittest.TestCase):
         self.assertEqual(len(pretty.split('\n')), 5)
 
     def test_unfinished(self):
-        players = [DummyPlayer(id=1)]
+        players = [DummyPlayer(playerId=1)]
         log = GameLog(players)
         pretty = log.pretty()
         print(pretty)

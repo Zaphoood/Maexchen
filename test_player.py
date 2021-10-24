3265da2fec6dda531308eb62b1a60f6dc72d3fd0
Mathis Weber <mathis.c.weber@web.de>
2021-10-24 12:31:53 +0200

    Adjust unittests in test_player.py to reflect new function signature

--- test_player.py
+++ test_player.py
@@ -1,4 +1,5 @@
 import unittest
+from random import Random
 
 import constants as c
 from player import DummyPlayer, RandomPlayer
@@ -7,25 +8,33 @@ from move import Move
 
 
 class TestDummyPlayer(unittest.TestCase):
+    def setUp(self) -> None:
+        self.rng = Random()  # random.Random()
+
     def test_get_move(self):
         dummy = DummyPlayer()
         # Eigenes Ergebnis soll wahrheitsgemäß verkündet werden, wenn es den Vorgänger übertrumpft
         throw = Throw(3, 1)
         while throw < Throw(2, 1):
-            bigger = throw + 1
-            throwStated = dummy.getThrow(bigger, throw)
-            self.assertEqual(throwStated, bigger)
+            biggerThrow = throw + 1
+            throwStated = dummy.getThrowStated(biggerThrow, throw, self.rng)
+            self.assertEqual(throwStated, biggerThrow)
             throw += 1
 
         # Vorgänger, der Mäxchen gewürfelt hat, soll immer angezweifelt werden.
-        move = dummy.getDoubt(Throw(2, 1))
-        self.assertEqual(move, Move(c.ALL_MOVES.DOUBT))
+        doubt = dummy.getDoubt(Throw(2, 1), self.rng)
+        self.assertTrue(doubt)
+
 
 class TestRandomPlayer(unittest.TestCase):
+    def setUp(self) -> None:
+        self.rng = Random()  # random.Random()
+
     def test_get_doubt(self):
         rand = RandomPlayer()
-        rand.getDoubt(Throw(2, 1))
-        rand.getThrowStated(Throw(4, 5), Throw(6, 1))
+        rand.getDoubt(Throw(2, 1), self.rng)
+        rand.getThrowStated(Throw(4, 5), Throw(6, 1), self.rng)
+
 
 if __name__ == '__main__':
     unittest.main()

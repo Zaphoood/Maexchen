805cd963c502df40ecd66280d796e14e68f82d62
Mathis Weber <mathis.c.weber@web.de>
2021-10-24 12:57:28 +0200

    Create unittests to ensure reproducibility of RandomPlayer behavior

--- test_player.py
+++ test_player.py
@@ -31,9 +31,37 @@ class TestRandomPlayer(unittest.TestCase):
         self.rng = Random()  # random.Random()
 
     def test_get_doubt(self):
-        rand = RandomPlayer()
-        rand.getDoubt(Throw(2, 1), self.rng)
-        rand.getThrowStated(Throw(4, 5), Throw(6, 1), self.rng)
+        randomPlayer = RandomPlayer()
+        randomPlayer.getDoubt(Throw(2, 1), self.rng)
+        randomPlayer.getThrowStated(Throw(4, 5), Throw(6, 1), self.rng)
+
+    def test_doubt_reproducibility(self):
+        fixed_seed = 12345
+        randomPlayer = RandomPlayer()
+
+        last_doubt = None
+        for i in range(1000):
+            # Assert that the player always makes the same decision using the same rng
+            self.rep_rng = Random(fixed_seed)
+            doubt = randomPlayer.getDoubt(Throw(2, 1), self.rep_rng)
+            if last_doubt == None:
+                last_doubt = doubt
+            else:
+                self.assertEqual(doubt, last_doubt)
+
+    def test_throw_reproducibility(self):
+        fixed_seed = 12345
+        randomPlayer = RandomPlayer()
+
+        last_throw = None
+        for i in range(1000):
+            # Assert that the player always makes the same decision using the same rng
+            self.rep_rng = Random(fixed_seed)
+            throw = randomPlayer.getThrowStated(Throw(2, 1), Throw(2, 1), self.rep_rng)
+            if last_throw is None:
+                last_throw = throw
+            else:
+                self.assertEqual(throw, last_throw)
 
 
 if __name__ == '__main__':

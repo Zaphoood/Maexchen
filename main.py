aad7d3d9765785d91b096b122b1140a061bcb6de
Zaphoood <mathis.c.weber@web.de>
2021-10-27 01:24:23 +0200

    Add command line argument parsing to main.py (verbosity, number of repetitions)

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+import sys
+from contextlib import suppress
+
 from game import Game
 from player import DummyPlayer, ShowOffPlayer
+from evaluate import Evaluation
+
+args = sys.argv[1:]
+n_reps = 1000
+verbose = False
+
+for i, arg in enumerate(args):
+    if arg == "-n":
+        with suppress(ValueError):  # contextlib.suppress
+            n_reps = int(args[i + 1])
+    elif arg == "-v":
+        verbose = True
+
+print(f"Running simulation {n_reps} times...")
 
 if __name__ == '__main__':
-    game = Game([*[DummyPlayer()] * 3, ShowOffPlayer()])
-
-    game.init()
-    game.run()
+    eval = Evaluation([DummyPlayer(), DummyPlayer(), ShowOffPlayer(), ShowOffPlayer()], n_reps, verbose=verbose)
+    eval.run()
+    print(eval.prettyResults())

3e35abb2f296236fd9df1a31cc9f755496b72425
Zaphoood <mathis.c.weber@web.de>
2021-10-28 21:15:38 +0200

    'Merge' only test_evaluate.py from master

--- test_evaluate.py
+++ test_evaluate.py
@@ -2,31 +2,22 @@ import unittest
 
 from evaluate import getWinner
 from game import Game
-from player import DummyPlayer, ShowOffPlayer
+from player import DummyPlayer, ShowOffPlayer, RandomPlayer
 from gamelog import GameLog
+from evaluate import Evaluation
 
 
 class TestEvaluate(unittest.TestCase):
-    def test_get_winner(self):
-        players = [DummyPlayer(playerId=0), DummyPlayer(playerId=1), DummyPlayer(playerId=2), ShowOffPlayer()]
-        seed = 8310664473561248720
-        self.game = Game(players, seed=seed)
-        # Spieler neu speichern, da ihnen von Game IDs zugewiesen wurden
-        players = self.game.players
-        self.game.init()
-        self.game.run()
-        print(f"Seed is {self.game.getSeed()}\n")
-        print(self.game.log.pretty())
-        # Bei diesem Seed gewinnt DummyPlayer mit id=2;
-        # Überprüfen von getWinner
-        self.assertEqual(getWinner(self.game.log), DummyPlayer(playerId=2))
+    def test_get_player_stats(self):
+        ev = Evaluation([DummyPlayer()], 0, verbose=False)
+        ev.getPlayerStats(0)
+        ev.run()
+        print(ev.prettyResults())
 
-    def test_get_winner_unfinished(self):
-        """getWinner() mit nicht beendetem Spiel testen."""
-        self.game = Game([DummyPlayer(), DummyPlayer()])
-        self.assertEqual(getWinner(self.game.log), None)
-        self.game.init()
-        self.assertEqual(getWinner(self.game.log), None)
+    def test_randoms(self):
+        ev = Evaluation([RandomPlayer() for _ in range(3)], 1000, verbose=False)
+        ev.run()
+        print(ev.prettyResults())
 
 
 if __name__ == '__main__':

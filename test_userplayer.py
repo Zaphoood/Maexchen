c0fcfea21a0afb1cc612f7e0a523950891174d18
Zaphoood <mathis.c.weber@web.de>
2021-10-29 12:05:58 +0200

    More detailed unittest for userplayer.py

--- test_userplayer.py
+++ test_userplayer.py
@@ -14,13 +14,17 @@ class TestUserPlayer(unittest.TestCase):
         p = UserPlayer()
         print(p.getDoubt(Throw(3, 1), 0, self.rng))
 
+    def test_get_throw_stated_first_round(self):
+        p = UserPlayer()
+        print(p.getThrowStated(Throw(5, 4), None, 0, self.rng))
+
     def test_get_throw_stated_beats(self):
         p = UserPlayer()
-        print(p.getThrowStated(Throw(5, 4), Throw(3, 2), 0, self.rng))
+        print(p.getThrowStated(Throw(5, 4), Throw(3, 2), 1, self.rng))
 
     def test_get_throw_stated_doesnt_beat(self):
         p = UserPlayer()
-        print(p.getThrowStated(Throw(5, 4), Throw(2, 1), 0, self.rng))
+        print(p.getThrowStated(Throw(5, 4), Throw(2, 1), 1, self.rng))
 
 
 if __name__ == "__main__":

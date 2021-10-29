ff34b562c74cf923dc22797e54969358410a2035
Zaphoood <mathis.c.weber@web.de>
2021-10-29 11:40:32 +0200

    Fix unittests for userplayer.py: new function signature

--- test_userplayer.py
+++ test_userplayer.py
@@ -12,15 +12,15 @@ class TestUserPlayer(unittest.TestCase):
 
     def test_get_doubt(self):
         p = UserPlayer()
-        print(p.getDoubt(Throw(3, 1), self.rng))
+        print(p.getDoubt(Throw(3, 1), 0, self.rng))
 
     def test_get_throw_stated_beats(self):
         p = UserPlayer()
-        print(p.getThrowStated(Throw(5, 4), Throw(3, 2), self.rng))
+        print(p.getThrowStated(Throw(5, 4), Throw(3, 2), 0, self.rng))
 
     def test_get_throw_stated_doesnt_beat(self):
         p = UserPlayer()
-        print(p.getThrowStated(Throw(5, 4), Throw(2, 1), self.rng))
+        print(p.getThrowStated(Throw(5, 4), Throw(2, 1), 0, self.rng))
 
 
 if __name__ == "__main__":

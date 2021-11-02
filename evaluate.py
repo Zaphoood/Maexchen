eadcc796f61ac09bd4508b1d8d63a334da31cf69
Zaphoood <mathis.c.weber@web.de>
2021-11-02 14:35:35 +0100

    Remove function getWinner() since it's not used

--- evaluate.py
+++ evaluate.py
@@ -18,22 +18,6 @@ class IncompleteLogError(Exception):
     pass
 
 
-def getWinner(log: GameLog) -> Player:
-    if log.hasFinished():
-        playersAlive = log.players
-        for kickEvent in [e for e in log.getEvents() if isinstance(e, EventKick)]:
-            for i, player in enumerate(playersAlive):
-                if player.id == kickEvent.playerId:
-                    playersAlive.pop(i)
-        if len(playersAlive) == 1:
-            return playersAlive[0]
-        else:
-            raise IncompleteLogError(f"Couldn't determine winner from GameLog. {len(playersAlive)} left at the end of "
-                                     f"evaluation.")
-    else:
-        return None
-
-
 class Evaluation:
     """Führt wiederholte Spielsimulationen durch."""
 

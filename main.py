1c2001581f23147624d37703d13c093c3b3d4bb9
Mathis Weber <mathis.c.weber@web.de>
2021-11-03 13:13:38 +0100

    Fix bug with command line argument parsing

--- main.py
+++ main.py
@@ -30,36 +30,43 @@ players = []
 if not args:
     print(c.START_SIM_USAGE)
     sys.exit(1)
-    
 try:
     n_reps = int(args[0])
+    args = args[1:]
 except (ValueError, IndexError) as e:
     print(f"\nExpected number of repetitions, got \"{args[0]}\"")
     sys.exit(1)
+skip_arg = False
 for i, arg in enumerate(args):
+    if skip_arg:
+        skip_arg = False
+        continue
     if arg == "-q":
         quiet = True
     elif arg == "-v":
         logging_level = logging.INFO
+    elif arg == "-x":
+        save_to_disk = False
     elif arg == "-p":
         plot_win_rate = True
         plot_loss_reason = True
-    elif arg.startswith("--"):
-        if arg == "--no-write":
-            write_to_disk = False
-        elif arg == "--plot-win-rate":
-            plot_win_rate = True
-        elif arg == "--plot-loss-reason":
-            plot_loss_reason = True
-        elif arg[2:] in ARGS_TO_PLAYERS.keys():
-            player_class = ARGS_TO_PLAYERS[arg[2:]]
-            p = player.__getattribute__(player_class)
-            n = 1
-            with suppress(ValueError, IndexError):  # contextlib.suppress
-                n = int(args[i + 1])
-            players.extend([p() for _ in range(n)])
-        else:
-            raise ValueError(f"Unexpected command line argument \"{arg}\"")
+    elif arg == "--no-write":
+        save_to_disk = False
+    elif arg == "--plot-win-rate":
+        plot_win_rate = True
+    elif arg == "--plot-loss-reason":
+        plot_loss_reason = True
+    elif arg.startswith("--") and arg[2:] in ARGS_TO_PLAYERS.keys():
+        player_class = ARGS_TO_PLAYERS[arg[2:]]
+        p = player.__getattribute__(player_class)
+        n = 1
+        with suppress(ValueError, IndexError):  # contextlib.suppress
+            n = int(args[i + 1])
+        players.extend([p() for _ in range(n)])
+        # Skip next arg
+        skip_arg = True
+    else:
+        raise ValueError(f"Unexpected command line argument \"{arg}\"")
 
 
 logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging_level)

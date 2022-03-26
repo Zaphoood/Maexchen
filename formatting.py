1037dcc6a667e9b5d1f4baef89f0c1cef8782161
Mathis Weber <mathis.c.weber@web.de>
2022-03-26 16:04:17 +0100

    Clean up formating.py

--- formatting.py
+++ formatting.py
@@ -1,24 +1,38 @@
-def formatTable(table, space=2) -> str:
-    """Eine zweidimensionale Tabelle formatieren und als String zurückgeben.
+from typing import List
 
-    :param table: Tabelle als nested list
-    :param space: Anzahl Leerzeichen zwischen zwei Spalten
-    :return: Tabelle als str"""
+def formatTable(table: List[List[str]], spaces: int = 2) -> str:
+    """Format a two-dimensional table into a string.
+
+    :param table: Two-dimensional list
+    :param spaces: Amount of spaces between columns
+    :return: str containing the formatted table
+    """
 
     assert all([len(row) == len(table[0]) for row in table[1:]]), "All rows must be of the same width"
     assert all([isinstance(el, str) for row in table for el in row]), "All elements must be strings"
 
-    delim = " " * space
-    column_widths = [max([len(table[row][col] + delim) for row in range(len(table))]) for col in range(len(table[0]))]
+    delim = " " * spaces
+    # The width of each column, which is determined by the length of its widest element
+    # plus the number of delimiting spaces between columns
+    column_widths: List[int] = [max([len(el) for el in col]) + spaces for col in zip(*table)]
     
-    table_str = "\n".join(
-            ["".join(
-                [el + " " * (column_widths[col] - len(el))
-                for col, el in enumerate(row)])
-            for row in table])
+    output = ""
+    for row in table:
+        for col, el in enumerate(row):
+            # Left-align text with additional spaces to the left, so that its
+            # total length is equal to column_widths[col]
+            output += ("{: <" + str(column_widths[col]) + "}").format(el)
+        output += "\n"
 
-    return table_str
+    return output
 
 
 def printProgress(prg: int, total: int, end: str = "\n"):
-    print("[" + "#"*prg + "."*(total-prg) + "]", end=end)
+    """Simple progress bar.
+
+    :param total: Inner width of the progress bar
+    :param prg: Progress
+    :param end: Optional. str to print after the progress bar
+    """
+    output = "[" + "#" * prg + "." * (total - prg) + "]"
+    print(output, end=end)

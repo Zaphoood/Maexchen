b614a3130ab6e93a6a9875ce95fe6beba7f17bea
Mathis Weber <mathis.c.weber@web.de>
2022-03-26 18:07:53 +0100

    Clean up main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from evaluate import Evaluation
 from argp import ArgumentParser
-import player
+from player import FLAGS_TO_PLAYERS
 from game import TooFewPlayers
 import logging
 from disk import existsPathToFile
@@ -11,16 +11,16 @@ parser = ArgumentParser()
 parser.parseArgs()
 
 def main_with_catch():
-    """Warp the program in a try/except block that catches KeyboardInterrupt and exits nicely."""
+    """Wrap the entire program in a try/except block that catches KeyboardInterrupt and exits nicely."""
     try:
         main()
     except KeyboardInterrupt:
         print("\nAborted")
 
-
 def main():
+    # Set up an Evaluation according to command line arguments
     players = []
-    for player_flag, player_class in player.FLAGS_TO_PLAYERS.items():
+    for player_flag, player_class in FLAGS_TO_PLAYERS.items():
         # If the user provided a value after the flag, it is stored in Flag.value -> Use this as the number of players
         # Otherwise, just add one player
         n = parser.getFlag(player_flag).value or (1 & parser.getFlag(player_flag).set)
@@ -38,24 +38,28 @@ def main():
                 logging.error(f"Can't write to {log_path}: Directory doesn't exist.")
                 exit(1)
  
+    # Perform the Evaluation
+    ev = Evaluation(players, parser.n_reps,
+                    show_progress=not parser.getFlag("quiet").set)
     try:
-        ev = Evaluation(players, parser.n_reps,
-                        show_progress=not parser.getFlag("quiet").set)
         ev.run()
-        ev.saveResultsToDisk(log_path=log_path)
-        print(ev.prettyResults(sort_by_winrate=not parser.getFlag("no-sort").set, force_rerender=True))
-
-        if parser.getFlag("plot-all").set:
-            ev.plotWRandLR()
-        else:
-            if parser.getFlag("plot-win-rate").set:
-                ev.plotWinRate()
-            if parser.getFlag("plot-loss-reason").set:
-                ev.plotLossReason()
     except TooFewPlayers as e:
         print(e.message)
         exit(1)
 
+    ev.saveResultsToDisk(log_path=log_path)
+    print(ev.prettyResults(sort_by_winrate=not parser.getFlag("no-sort").set, force_rerender=True))
+
+    # Plot the results
+    if parser.getFlag("plot-all").set:
+        ev.plotWRandLR()
+    else:
+        if parser.getFlag("plot-win-rate").set:
+            ev.plotWinRate()
+        if parser.getFlag("plot-loss-reason").set:
+            ev.plotLossReason()
+
 
 if __name__ == '__main__':
     main_with_catch()
+
